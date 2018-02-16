7cf60a6e1cb44f3f87dff8b359e5458b88a983d1
jz1242 <jz1242@gmail.com>
2018-02-15 22:55:52 -0500

    stf

--- hw1/iPerfer.cpp
+++ hw1/iPerfer.cpp
@@ -16,20 +16,6 @@
 #define BACKLOG 10
 #define MAXSIZE 1000
 
-void sigchld_handler(int s)
-{
-	(void)s; // quiet unused variable warning
-
-	// waitpid() might overwrite errno, so we save and restore it:
-	int saved_errno = errno;
-
-	while(waitpid(-1, NULL, WNOHANG) > 0);
-
-	errno = saved_errno;
-}
-
-
-// get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
 	if (sa->sa_family == AF_INET) {
@@ -103,14 +89,10 @@ int main(int argc, char** argv){
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    hints.ai_flags = AI_PASSIVE;
 
-    if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
-    }
+    rv = getaddrinfo(NULL, port, &hints, &servinfo);
 
-    // loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
@@ -129,42 +111,21 @@ int main(int argc, char** argv){
             perror("server: bind");
             continue;
         }
-
         break;
     }
 
-    freeaddrinfo(servinfo); // all done with this structure
+    freeaddrinfo(servinfo);
 
     if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
 
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
-    }
-
-    sa.sa_handler = sigchld_handler; // reap all dead processes
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_RESTART;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
-        perror("sigaction");
-        exit(1);
-    }
-    printf("server: waiting for connections...\n");
+    listen(sockfd, BACKLOG);
     sin_size = sizeof their_addr;
     new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-    if (new_fd == -1) {
-      perror("accept");            
-    }
-
-    inet_ntop(their_addr.ss_family,
-        get_in_addr((struct sockaddr *)&their_addr),
-        s, sizeof s);
-    printf("server: got connection from %s\n", s);  
+    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
     gettimeofday(&tv1, NULL);
-    while(1) {  // main accept() loop  
+    while(1) {
       if(buf[numbytes - 1] == '1'){
         gettimeofday(&tv2, NULL);
         char end[MAXSIZE];
@@ -182,10 +143,7 @@ int main(int argc, char** argv){
         return 0;
       }
 
-      if ((numbytes = recv(new_fd, buf, MAXSIZE, 0)) == -1) {
-        perror("recv");
-        exit(1);
-      }
+      numbytes = recv(new_fd, buf, MAXSIZE, 0);
       buf[numbytes] = '\0';
       totalbytes += numbytes;
 
@@ -198,13 +156,9 @@ int main(int argc, char** argv){
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
+    
+    rv = getaddrinfo(argv[3], port, &hints, &servinfo);
 
-    if ((rv = getaddrinfo(argv[3], port, &hints, &servinfo)) != 0) {
-      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-      return 1;
-    }
-
-    // loop through all the results and connect to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
       if ((sockfd = socket(p->ai_family, p->ai_socktype,
           p->ai_protocol)) == -1) {
@@ -226,9 +180,7 @@ int main(int argc, char** argv){
       return 2;
     }
 
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
-        s, sizeof s);
-    printf("client: connecting to %s\n", s);
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
 
     freeaddrinfo(servinfo); // all done with this structure
     gettimeofday(&tv1, NULL);
@@ -237,7 +189,6 @@ int main(int argc, char** argv){
       numbytes = send(sockfd, bufsend, MAXSIZE, 0);
       totalbytes += numbytes;
       gettimeofday(&tv2, NULL);
-      //printf("loop time is : %s", ctime(&start));
     }
     char end[MAXSIZE];
     memset(end, '1', sizeof(end));
